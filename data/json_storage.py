c61a88eaff3749d72c3da11e66c59ebb8d148d79
lilsneq <bilmork2407@gmail.com>
2026-04-20 02:10:32 +0300

    ОБНОВИЛ JSON сохранение так как было завязано на tkinker

--- data/json_storage.py
+++ data/json_storage.py
@@ -1,53 +1,51 @@
 #Импорты
 import json
-from tkinter import filedialog
-from datetime import datetime
-import os
+from config.settings import APPLICATIONS_FILE
 
 
 
 
 
 class JSONStorage:
+
+    def __init__(self, default_path=APPLICATIONS_FILE):
+        self.default_path = default_path
+
+
     def save_data_json(self, data, filepath=None):
+        #СОХРАНЕНИЕ ИЗ JSON
+        target_path = filepath or self.default_path
+
         try:
-            if filepath is None:
-                filepath = filedialog.asksaveasfilename(defaultextension=".json")
-
-                if not filepath:
-                    return False, "Файл не выбран"
-
-            with open(filepath, "w", encoding='utf-8') as f:
+            with open(target_path, "w", encoding='utf-8') as f:
                 json.dump(data, f, indent=4, ensure_ascii=False)
-                return True, f'Данные успешно сохранены в {filepath}'
+                return True, f'Данные успешно сохранены в {target_path}'
 
         except TypeError as e:
-            # Ошибка несериализуемого типа
-            return False, f"Ошибка типа данных: {str(e)}"
+            return False, f"Ошибка типа данных: {e}"
 
         except (PermissionError, OSError) as e:
-            return False, f'Ошибка файловой системы: {str(e)}'
+            return False, f'Ошибка файловой системы: {e}'
 
         except Exception as e:
-            return False, f'Непредвиденная ошибка: {str(e)}'
+            return False, f'Непредвиденная ошибка: {e}'
 
 
     def load_data(self, filepath=None):
+        #загрузка из файла JSON
+        target_path = filepath or self.default_path
         try:
-            if filepath is None:
-                filepath = filedialog.askopenfilename()
-                if not filepath:
-                    return False, "Файл не выбран"
-
-            with open(filepath, "r", encoding='utf-8') as f:
+            with open(target_path, "r", encoding='utf-8') as f:
                 data = json.load(f)
                 return True, data
+        except FileNotFoundError as e:
+            return False, f'Файл не найден {e}'
 
         except TypeError as e:
-            return False, f"Ошибка типа данных: {str(e)}"
+            return False, f"Ошибка типа данных: {e}"
 
         except Exception as e:
-            return False, f'Ошибка: {str(e)}'
+            return False, f'Ошибка: {e}'
 
 
 
