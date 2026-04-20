dbd1274671e73c4c5b3b553f7b5f2e9388dccbdd
lilsneq <bilmork2407@gmail.com>
2026-04-20 20:18:08 +0300

    МОДУЛЬ КОТОРЫЙ ПОКАЗЫВАЕТ ТАБЛИЦУ БД

--- database/db_find_operations.py
+++ database/db_find_operations.py
@@ -1,6 +1,42 @@
 #МОДУЛЬ ПОИСКА
-from database_connect import get_connection
+from database.database_connect import get_connection
+
+class ViewAllOperations:
+
+    @staticmethod
+    def find_product():
+        conn = get_connection()
+        if not conn:
+            return []
+
+        query = """
+            SELECT * FROM public.warehouse
+            ORDER BY id ASC;
+            """
+        print("ЗАПРОС В БАЗУ ДАННЫХ ОТПРАВЛЕН")
+
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                rows = cursor.fetchall()
+                return rows
+
+        except Exception as e:
+            print(f'ОШИБКА ПОЛУЧЕНИЯ ДАННЫХ {e}')
+            return []
+        finally:
+            conn.close()
 
 
-def find_product(cursor, company, warehouse, product):
-    pass
+
+
+if __name__ == '__main__':
+    rows = ViewAllOperations.find_product()
+    col_widths = [max(len(str(row[i])) for row in rows) for i in range(len(rows[0]))]
+
+    # 2. Печатаем строки
+    for row in rows:
+        # ljust(col_widths[i]) выравнивает по левому краю с нужным отступом
+        formatted_row = " | ".join(str(item).ljust(col_widths[i]) for i, item in enumerate(row))
+        print(f"| {formatted_row} |")
+
