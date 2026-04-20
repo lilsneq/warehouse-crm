854cfca5465d5607d437e5d11d2866460bb5e998
lilsneq <bilmork2407@gmail.com>
2026-04-20 20:17:44 +0300

    МОДУЛЬ SQL ПРОВЕРКИ ИМЕЕТСЯ ЛИ ТАКОЙ ТОВАР В БД

--- database/db_checks.py
+++ database/db_checks.py
@@ -1,44 +1,53 @@
 #МОДУЛЬ ПРОВЕРОК ВХОЖДЕНИЙ
+
 import psycopg2
-from database_connect import get_connection
+from database.database_connect import get_connection
 from datetime import datetime
 
 
-now = datetime.now()
 
+class CheckProduct:
+    # КЛАСС ПРОВЕРКИ ТОВАРА В БД ПО 4 КРИТЕРИЯМ
+    @staticmethod
+    def check_exists(company, warehouse, category, product):
+        """ПРОВЕРКА ВХОЖДЕНИЯ ТОВАРА В БАЗУ ДАННЫХ"""
+        now = datetime.now()
+        conn = None
 
-def check_exists(company, warehouse, product):
-    """ПРОВЕРКА ВХОЖДЕНИЯ ТОВАРА В БАЗУ ДАННЫХ"""
-    conn = None
+        try:
+            conn = get_connection()
+            if not conn:
+                return False
 
-    try:
-        conn = get_connection()
-        if not conn:
+            query = """
+                SELECT 1 FROM public.warehouse
+                WHERE name_company = %s 
+                    AND name_warehouse = %s
+                    AND name_category = %s
+                    AND name_product = %s
+                LIMIT 1;
+            """
+            print(f'Запрос отправлен в базу данных', now)
+
+            with conn.cursor() as cursor:
+                cursor.execute(query, (company, warehouse, category, product))
+                exists = cursor.fetchone() is not None
+                if cursor.rowcount == 0:
+                    print('ТОВАР НЕ НАЙДЕН НИЧЕГО НЕ ОБНОВЛЕННО')
+                    return False
+                return exists
+
+        except Exception as e:
+            print(f'ОШИБКА МОДУЛЯ ПРОВЕРКИ check.py - {e}', now)
             return False
-
-        query = """
-            SELECT 1 FROM public.warehouse
-            WHERE name_company = %s AND name_warehouse = %s AND name_product = %s
-            LIMIT 1;
-        """
-        print(f'Запрос отправлен в базу данных', now)
-
-        with conn.cursor() as cursor:
-            cursor.execute(query, (company, warehouse, product))
-            exists = cursor.fetchone() is not None
-            return exists
-
-    except Exception as e:
-        print(f'ОШИБКА МОДУЛЯ ПРОВЕРКИ check.py - {e}', now)
-        return False
-    finally:
-        if conn:
-            conn.close()
+        finally:
+            if conn:
+                conn.close()
 
 
 
 if __name__ == '__main__':
     #ТЕСТ ПРОВЕРКА ВХОЖДЕНИЯ ТОВАРА
-    test_res = check_exists('LG', 'Основной', 'ВИЛКА')
-    print(f"Результат проверки: {'Найдено' if test_res else 'Не найдено'}")
+    res = check_exists('LG', 'Склад_1', 'ТВ', 'OLED55')
+    print(f"Обновление успешно: {res}")
 
